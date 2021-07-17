6afab376366bce52acae544a0c6aa97125651427
anfederico <anfederico@users.noreply.github.com>
2021-07-17 12:41:14 -0400

    Interactive community analyses

--- R/enrichment.R
+++ R/enrichment.R
@@ -1,5 +1,10 @@
 #' Distinct colors for large categorical datasets
 #' 
+#' Colors derived from the R package colorspace
+#' colorspace: A Toolbox for Manipulating and Assessing Colors and Palettes
+#' https://cran.r-project.org/web/packages/colorspace/index.html
+#' License: BSD_3_clause + https://cran.r-project.org/web/packages/colorspace/LICENSE
+#' 
 #' @param n Number of colors desired
 #' 
 #' @return Color palette
@@ -7,15 +12,14 @@
 #' @export
 distinct_colors <- function(n) {
     x <- c("#353E7C","#007094","#009B95","#00BE7D","#96D84B","#FDE333","#040404","#3E134F","#851170","#C53270",
-          "#F36E35","#F8B83C","#A23D47","#A96C00","#9A9800","#75C165","#50E2BB","#B0F4FA","#26185F","#005D9E",
-          "#18BDB0","#9ADCBB","#D7F4CF","#DD008F","#DB6AC0","#D5A6DB","#F6F6FC","#CBA079","#928261","#605F4C",
-          "#363932","#001889","#87008D","#DAFF47","#88002D","#FE945C","#FFE2C0","#004533","#006F69","#0091AD",
-          "#EDD788","#AB4A3D","#73243C","#AC0535","#EB2C31","#EF4868","#F56553","#404E9C","#3D7CB8","#4BA5BF",
-          "#55C7B1","#A3E292","#FAEF8B","#0A1230","#3C2871","#7A3392","#B7509A","#E68375","#F1C687","#985277",
-          "#A37B49","#9BA453","#86CBA0","#7DEBEA","#C0FCFC","#2C2C7D","#396ABC","#5DC6DB","#AAE6EA","#DEFDFD",
-          "#CA40B4","#CF82E3","#D1B6F3","#F7FFFF","#C7AEAE","#928F93","#626C7B","#3A465F","#25309C","#7B31A9",
-          "#DEFD99","#7D245B","#F3A698","#FDF0F3","#215061","#367A94","#4B9CD2","#EBE4C4","#A15D70","#6B3868",
-          "#9F2D65","#DB4D6A","#DF6598","#E77C8B")
+           "#F36E35","#F8B83C","#A23D47","#A96C00","#9A9800","#75C165","#50E2BB","#B0F4FA","#26185F","#005D9E",
+           "#18BDB0","#9ADCBB","#D7F4CF","#DD008F","#DB6AC0","#D5A6DB","#F6F6FC","#CBA079","#928261","#605F4C",
+           "#363932","#001889","#87008D","#DAFF47","#88002D","#FE945C","#FFE2C0","#004533","#006F69","#0091AD",
+           "#EDD788","#AB4A3D","#73243C","#AC0535","#EB2C31","#EF4868","#F56553","#404E9C","#3D7CB8","#4BA5BF",
+           "#55C7B1","#A3E292","#FAEF8B","#0A1230","#3C2871","#7A3392","#B7509A","#E68375","#F1C687","#985277",
+           "#A37B49","#9BA453","#86CBA0","#7DEBEA","#C0FCFC","#2C2C7D","#396ABC","#5DC6DB","#AAE6EA","#DEFDFD",
+           "#CA40B4","#CF82E3","#D1B6F3","#F7FFFF","#C7AEAE","#928F93","#626C7B","#3A465F","#25309C","#7B31A9",
+           "#DEFD99","#7D245B","#F3A698","#FDF0F3","#215061","#367A94","#4B9CD2","#EBE4C4","#A15D70","#6B3868")
     return(rep(x, floor(n/length(x))+1)[1:n])
 }
 
@@ -57,38 +61,18 @@ colorize <- function(values, resolution=4) {
 #' 
 #' @return Propagation values
 #' 
-#' @import dnet
+#' @importFrom dnet dRWR
+#' @importFrom igraph V vcount vertex_attr_names
 #' @export
 do_rwr <- function(ig, seeds, restart=0.5, normalise="laplacian", verbose=FALSE, ...) {
-    mat <- matrix(0, nrow=vcount(ig), ncol=1)
+    stopifnot("name" %in% igraph::vertex_attr_names(ig))
+    mat <- matrix(0, nrow=igraph::vcount(ig), ncol=1)
     rownames(mat) <- V(ig)$name
     mat[,1] <- as.integer(rownames(mat) %in% seeds)
     rwr <- dnet::dRWR(g=ig, normalise=normalise, setSeeds=mat, restart=restart, verbose=verbose, ...)
     p <- rwr[,1]
     names(p) <- rownames(mat)
     return(p)
-}
-
-#' Extend a signature in the context of a network
-#' 
-#' @param ig An igraph object
-#' @param signature A character vector of symbols
-#' @param restart Restart probability between 0-1
-#' 
-#' @return An igraph and an extended signature
-#' 
-#' @import hypeR
-#' @import igraph
-#' @export
-do_signature <- function(ig, signature, restart=0.5) {
-    seeds <- intersect(signature, V(ig)$name)
-    stopifnot(length(seeds) > 0)
-    rwr <- do_rwr(ig, seeds=seeds, restart=restart)
-    V(ig)$p <- rwr[match(V(ig)$name, names(rwr))]
-    V(ig)$pcolor <- colorize(V(ig)$p)
-    signature.ranked <- rwr[order(-rwr)]
-    data = list(ig=ig, signature=signature.ranked)
-    return(data)
 }
 
 #' Do enrichment for a single community
@@ -103,17 +87,35 @@ do_signature <- function(ig, signature, restart=0.5) {
 #' 
 #' @return An igraph and hyp object
 #' 
-#' @import hypeR
-#' @import igraph
+#' @importFrom hypeR hypeR
+#' @importFrom dplyr %>% arrange pull
+#' @importFrom igraph V vertex_attr_names as_data_frame
 #' @export
-do_community <- function(ig, community, genesets, restart=0.5, fdr=1, pval=1, plotting=FALSE) {
+enrich_community <- function(ig, community, genesets, restart=0.5, fdr=1, pval=1, plotting=FALSE) {
+    stopifnot("symbol" %in% igraph::vertex_attr_names(ig))
+    stopifnot("community" %in% igraph::vertex_attr_names(ig))
+    
+    # Extract node identifiers
     seeds <- V(ig)$name[V(ig)$community == community]
     rwr <- do_rwr(ig, seeds=seeds, restart=restart)
-    V(ig)$p <- rwr[match(V(ig)$name, names(rwr))]
+    
+    # Map node identifiers to propagation
+    v.where <- match(V(ig)$name, names(rwr))
+    
+    # Copy over data
+    V(ig)$p <- rwr[v.where]
     V(ig)$pcolor <- colorize(V(ig)$p)
-    signature.ranked <- names(rwr[order(-rwr)])
+    
+    # Use symbols for the signature
+    signature.ranked <-
+        igraph::as_data_frame(ig, what="vertices") %>%
+        dplyr::arrange(desc(p)) %>%
+        dplyr::pull(symbol)
+    
+    # Ranked-based enrichment
     hyp <- hypeR(signature.ranked, genesets, test="kstest", pval=pval, fdr=fdr, plotting=plotting)
     data <- list(ig=ig, hyp=hyp)
+    
     return(data)
 }
 
@@ -124,30 +126,198 @@ do_community <- function(ig, community, genesets, restart=0.5, fdr=1, pval=1, pl
 #' @param restart Restart probability between 0-1
 #' @param fdr Passed to hypeR
 #' @param pval Passed to hypeR
+#' 
+#' @return A list of hyp objects
+#' 
+#' @importFrom igraph V vertex_attr_names
+#' @export
+enrich_communities <- function(ig, genesets, restart=0.5, fdr=1, pval=1) {
+    stopifnot("symbol" %in% igraph::vertex_attr_names(ig))
+    stopifnot("community" %in% igraph::vertex_attr_names(ig))
+
+    # Enrichment for each community
+    communities.unique <- unique(V(ig)$community)
+    communities.hyp <- mapply(function(community) {
+        data <- enrich_community(ig=ig, community=community, genesets=genesets, restart=restart, fdr=fdr, pval=pval)
+        return(data$hyp)
+    }, communities.unique, SIMPLIFY=FALSE, USE.NAMES=TRUE)
+    names(communities.hyp) <- paste("C", communities.unique, sep="@")
+    return(communities.hyp)
+}
+
+
+#' Labelize community enrichment
+#' 
+#' @param communities.hyp A list of hyp objects
 #' @param top Limit number of genesets shown
+#' @param sep label delimeter
+#' @param val A value to associate with genesets
+#' 
+#' @return A list of community enrichment labels
+#' 
+#' @export
+labelize_communities <- function(communities.hyp, top=1, sep="\n", val=NULL) {
+    communities.labels <- mapply(function(hyp) {
+        if (nrow(hyp$data) == 0) return("")
+        rows <- hyp$data[1:min(nrow(hyp$data), top),]
+        labs <- rows$label
+        if (!is.null(val)) {
+            if (val %in% colnames(rows)) {
+                labs <- paste(rows$label, " (", val, "=", rows[,val,drop=TRUE], ")", sep="")
+            }
+        }
+        labs <- paste(labs, collapse=sep)
+        return(labs)
+    }, communities.hyp, SIMPLIFY=FALSE, USE.NAMES=TRUE)
+    return(communities.labels)
+}
+
+#' Static enrichment for all communities of the network
+#' 
+#' @param ig An igraph object
+#' @param genesets A named list of genesets
+#' @param restart Restart probability between 0-1
+#' @param fdr Passed to hypeR
+#' @param pval Passed to hypeR
+#' @param top Limit number of genesets shown
+#' @param val A value to associate with genesets
 #' 
 #' @return An igraph object
 #' 
-#' @import igraph
+#' @importFrom igraph V
 #' @export
-do_enrichment <- function(ig, genesets, restart=0.5, fdr=1, pval=1, top=1) {
-    communities.unique <- unique(V(ig)$community)
-    labels <- mapply(function(community) {
-        data <- do_community(ig=ig, communit=community, genesets=genesets, restart=restart, fdr=fdr, pval=pval)
-        hyp <- data$hyp
-        pathways <- hyp$data$label[1:min(nrow(hyp$data), top)]
-        pathways <- ifelse(is.na(pathways), "", pathways)
-        pathways <- paste(pathways, collapse="\n")
-        return(pathways)
-    }, communities.unique, SIMPLIFY=FALSE, USE.NAMES=TRUE)
-    names(labels) <- paste("C", communities.unique, sep="@")
-    V(ig)$annotation = ""
-    for (label in names(labels)) {
-        community <- strsplit(label, "@")[[1]][[2]]
-        annotation <- labels[[label]]
+enrich_communities_static <- function(ig, genesets, restart=0.5, fdr=1, pval=1, top=1, val=NULL) {
+    communities.hyp <- enrich_communities(ig, genesets, restart=restart, fdr=fdr, pval=pval)
+    communities.labels <- labelize_communities(communities.hyp, top=top, sep="\n", val=val)
+    V(ig)$enrichment = ""
+    for (lab in names(communities.labels)) {
+        community <- strsplit(lab, "@")[[1]][[2]]
+        enrichment <- communities.labels[[lab]]
         centrality <- igraph::degree(ig)[V(ig)$community == community]
         central <- names(sort(centrality, decreasing=TRUE))[1]
-        V(ig)$annotation[V(ig)$name == central] <- annotation
+        V(ig)$enrichment[V(ig)$name == central] <- enrichment
     }
+    return(ig)
+}
+
+#' Interactive enrichment for all communities of the network
+#' 
+#' @param ig An igraph object
+#' @param genesets A named list of genesets
+#' @param restart Restart probability between 0-1
+#' @param fdr Passed to hypeR
+#' @param pval Passed to hypeR
+#' @param top Limit number of genesets shown
+#' @param val A value to associate with genesets
+#' 
+#' @return An visnetwork object
+#' 
+#' @importFrom dplyr %>%
+#' @importFrom visNetwork toVisNetworkData visNetwork visPhysics visEdges
+#' @export
+enrich_communities_interactive <- function(ig, genesets, restart=0.5, fdr=1, pval=1, top=1, val=NULL) {
+    communities.hyp <- enrich_communities(ig, genesets, restart=restart, fdr=fdr, pval=pval)
+    communities.labels <- labelize_communities(communities.hyp, top=top, sep="<br>", val=val)
+    
+    vd <- visNetwork::toVisNetworkData(ig)
+    nodes <- vd$nodes
+    edges <- vd$edges
+
+    # Community labeling
+    nodes$title <- sapply(nodes$community, function(x) {
+        community <- paste("C", x, sep="@")
+        enrichment <- communities.labels[[community]]
+        return(paste(paste("<b>C", x, "</b>", sep=""), enrichment, sep="<br>"))
+    })
+
+    visNetwork(nodes, edges) %>%
+    visPhysics(stabilization=FALSE) %>%
+    visEdges(smooth=FALSE)
+}
+
+#' Meta interactive enrichment for all communities of the network
+#' 
+#' @param ig An igraph object
+#' @param genesets A named list of genesets
+#' @param restart Restart probability between 0-1
+#' @param fdr Passed to hypeR
+#' @param pval Passed to hypeR
+#' @param top Limit number of genesets shown
+#' @param mse Minimum shared edges threshold to visualize
+#' @param val A value to associate with genesets
+#' 
+#' @return An visnetwork object
+#' 
+#' @importFrom dplyr %>% mutate select distinct
+#' @importFrom tibble deframe
+#' @importFrom visNetwork toVisNetworkData visNetwork visPhysics visEdges
+#' @importFrom igraph V as_data_frame graph_from_adjacency_matrix
+#' @export
+enrich_communities_interactive_meta <- function(ig, genesets, restart=0.5, fdr=1, pval=1, top=1, mse=-inf, val=NULL) {
+    communities.hyp <- enrich_communities(ig, genesets, restart=restart, fdr=fdr, pval=pval)
+    communities.labels <- labelize_communities(communities.hyp, top=top, sep="\n", val=val)
+    
+    # Calculate edges between communities
+    mat <- matrix(0, nrow=length(communities.labels), ncol=length(communities.labels))
+    rownames(mat) <- colnames(mat) <- names(communities.labels)
+    for (ir in rownames(mat)) {
+        for (ic in colnames(mat)) {
+            c1 <- as.numeric(strsplit(ir, "@")[[1]][[2]])
+            c2 <- as.numeric(strsplit(ic, "@")[[1]][[2]])
+            c1.v <- igraph::V(ig)[igraph::V(ig)$community == c1]
+            c2.v <- igraph::V(ig)[igraph::V(ig)$community == c2]
+            mat[ir,ic] <- length(E(ig)[to(c1.v) & from(c2.v)])
+            mat[ic,ir] <- length(E(ig)[to(c2.v) & from(c1.v)])
+        }
+    }
+    if (!base::isSymmetric(mat)) stop("Expected an undirected graph")
+    diag(mat) <- 0
+
+    # Remove weights below minimum shared edges threshold
+    mat[mat < mse] <- 0
+
+    ig.c <- igraph::graph_from_adjacency_matrix(mat, mode="undirected", weighted=TRUE, diag=FALSE)
+    vd.c <- visNetwork::toVisNetworkData(ig.c)
+
+    nodes <- vd.c$nodes
+    edges <- vd.c$edges
+
+    color.mapping <- igraph::as_data_frame(ig, what="vertices") %>%
+        dplyr::mutate(community=paste("C", community, sep="@")) %>%
+        dplyr::select(community, color) %>%
+        dplyr::distinct(community, color) %>%
+        tibble::deframe()
+        
+    nodes$color <- color.mapping[nodes$id]
+
+    # Community labeling
+    nodes$label <- sapply(nodes$id, function(x) {
+        enrichment <- communities.labels[[x]]
+        return(paste(gsub("@", "", x), enrichment, sep="\n"))
+    })
+
+    edges$color <- "grey"
+    edges$width <- edges$weight
+
+    visNetwork(nodes, edges) %>%
+    visPhysics(stabilization=FALSE) %>%
+    visEdges(smooth=FALSE, color="grey")
+}
+
+#' Extend a signature in the context of a network
+#' 
+#' @param ig An igraph object
+#' @param signature A character vector of node identifiers
+#' @param restart Restart probability between 0-1
+#' 
+#' @return An igrpah object
+#' 
+#' @export
+extend_signature <- function(ig, signature, restart=0.5) {
+    seeds <- V(ig)$name[match(signature, V(ig)$symbol)]
+    stopifnot(length(seeds) > 0)
+    rwr <- do_rwr(ig, seeds=seeds, restart=restart)
+    V(ig)$p <- rwr[match(V(ig)$name, names(rwr))]
+    V(ig)$pcolor <- colorize(V(ig)$p)
     return(ig)
 }
--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 
 # hypeR Network Extension
 
+[![hypeR](https://img.shields.io/badge/montilab-hypeR-0770d9?labelColor=000000)](https://github.com/montilab/hypeR)
 [![](https://lifecycle.r-lib.org/articles/figures/lifecycle-experimental.svg)](https://www.tidyverse.org/lifecycle/#experimental)
 [![](https://img.shields.io/github/last-commit/montilab/hypeR-next.svg)](https://github.com/montilab/hypeR-next/commits/master)
 
@@ -14,43 +15,41 @@ An extension of hypeR for network enrichment through random walks.
 devtools::install_github("montilab/hypeR-next")
 ```
 
-### Simulated Network Communities
-
-To get a sense of how this works, there is a simulated network with
-modular scale-free properties available. The object already has
-pre-defined communities that have been populated with genes from highly
-specific biological pathways. This data was generated through the
-following steps:
-
-1.  Simulate a graph structure through the
-    Lancichinetti–Fortunato–Radicchi (LFR) benchmark model
-2.  Detect and define communities within the graph
-3.  For each community sample node labels from a biological pathway
-    (80%) / random (20%)
-
-<!-- end list -->
-
-``` python
-LFR(n=600, tau1=2.7, tau2=2, mu=0.1, min_community=25, max_community=75)
-```
+### Data
 
 ``` r
 data(ig)
-print(ig)
 ```
 
-    #> IGRAPH c935075 UN-- 600 1497 -- 
-    #> + attr: community (v/n), color (v/c), label (v/c), name (v/c)
-    #> + edges from c935075 (vertex names):
-    #>  [1] GMPS  --PPIA    GMPS  --ACLY    GMPS  --GAPDH   GMPS  --ARG1   
-    #>  [5] GMPS  --ITGB8   PDLIM1--G6PD    PDLIM1--PRDX4   PDLIM1--PRDX6  
-    #>  [9] PDLIM1--ABCC1   PDLIM1--AHSP    PDLIM1--ANGPTL4 JUNB  --SOD2   
-    #> [13] JUNB  --CAT     JUNB  --IL10RB  JUNB  --AHSP    RER1  --NAPA   
-    #> [17] RER1  --NAPG    RER1  --COG2    RER1  --SCRN1   RER1  --COPE   
-    #> [21] RER1  --PPT1    RER1  --ABCA1   RER1  --SIAH2   RER1  --MAST4  
-    #> [25] ICAM1 --PIK3R5  ICAM1 --NUDT21  ICAM1 --GNAI3   ICAM1 --CD40   
-    #> [29] ICAM1 --SYT11   NPFFR2--CXCL8   NPFFR2--SLC31A2 NPFFR2--IL1R1  
-    #> + ... omitted several edges
+This package works with undirected `igraph` objects or extensions of
+`igraph` objects.
+
+``` r
+is(ig)
+```
+
+    #> [1] "igraph"
+
+The following vertex attributes are expected at a minimum.
+
+  - `name` - Unique node labels
+  - `symbol` - Node symbols corresponding to the genesets you use  
+  - `community`- Pre-detected community labels  
+  - `color` - Community-specific colors
+
+<!-- end list -->
+
+``` r
+head(igraph::as_data_frame(ig, what="vertices"))
+```
+
+    #>        community   color symbol   name
+    #> GMPS           8 #3E134F   GMPS   GMPS
+    #> PDLIM1        15 #9A9800 PDLIM1 PDLIM1
+    #> JUNB          15 #9A9800   JUNB   JUNB
+    #> RER1          17 #50E2BB   RER1   RER1
+    #> ICAM1         14 #A96C00  ICAM1  ICAM1
+    #> NPFFR2        14 #A96C00 NPFFR2 NPFFR2
 
 ``` r
 set.seed(1)
@@ -63,7 +62,7 @@ layout <- igraph::layout_with_graphopt(ig,
                                        spring.constant=1, 
                                        max.sa.movement=5)
 
-par(mar=c(0,0,0,0), mfrow=c(1,2))
+par(mar=c(0,0,0,0))
 plot(ig,
      vertex.size=4,
      vertex.color="grey",
@@ -71,80 +70,11 @@ plot(ig,
      vertex.label=NA,
      edge.width=1,
      layout=layout)
-
-plot(ig,
-     vertex.size=4,
-     vertex.color="#EEEEEE",
-     vertex.frame.color=adjustcolor("#000000", alpha.f=0.2),
-     vertex.label=V(ig)$name,
-     vertex.label.family="Helvetica",
-     vertex.label.color="black",
-     vertex.label.font=2,
-     vertex.label.cex=0.4,
-     vertex.label.dist=0,
-     edge.width=1,
-     layout=layout)
 ```
 
-![](README_files/figure-gfm/unnamed-chunk-5-1.png)<!-- -->
+![](README_files/figure-gfm/unnamed-chunk-7-1.png)<!-- -->
 
-The only thing your graph needs is unique node symbols that correspond
-to the genesets you’re testing against.
-
-``` r
-head(V(ig)$name)
-```
-
-    #> [1] "GMPS"   "PDLIM1" "JUNB"   "RER1"   "ICAM1"  "NPFFR2"
-
-You need to perform some type of community detection. There are many
-algorithms but I prefer defining community structure through random
-walks. Walktrap looks for densely connected subgraphs through random
-walks at varying lengths. Whatever algorithm you use, make sure to label
-the nodes with their designated community through `V(ig)$community`.
-
-``` r
-set.seed(1)
-ig.c <- igraph::walktrap.community(ig, steps=10)
-V(ig)$community <- ig.c$membership
-```
-
-``` r
-table(V(ig)$community)
-```
-
-    #> 
-    #>  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 
-    #> 24 42 33 41 37 34 29 25  4 66 43 55 27 26 33 45 36
-
-We can visualize the communities which seem well-defined. Not
-surprisingly since this example data was defined to be particularly
-modular for demonstration purposes.
-
-``` r
-par(mar=c(0,0,0,0))
-plot(ig,
-     vertex.size=5,
-     vertex.label=V(ig)$community,
-     vertex.label.family="Helvetica",
-     vertex.label.color="white",
-     vertex.label.font=2,
-     vertex.label.cex=0.6,
-     vertex.label.dist=0,  
-     edge.width=1,
-     layout=layout)
-```
-
-![](README_files/figure-gfm/unnamed-chunk-9-1.png)<!-- -->
-
-### Community Enrichment
-
-Enrichment is performed independently for each community. Rather than
-performing enrichment just on the community members, we want to account
-for neighboring members outside but within close proximity to the
-community. Therefore we perform network propagation seeded with the
-community neighbors and performing enrichment on highly traversed
-portions of the graph.
+### Network characterization through random walks
 
 ``` r
 library(hypeR)
@@ -152,216 +82,27 @@ genesets <- hypeR::msigdb_gsets("Homo sapiens", "H", clean=TRUE)
 ```
 
 ``` r
-data <- do_community(ig, community="16", genesets, restart=0.5, plotting=TRUE)
+ig.e <- enrich_communities_static(ig, genesets, restart=0.5, fdr=0.01, top=1)
 ```
 
 ``` r
 par(mar=c(0,0,0,0))
-ig.c <- data$ig
-plot(ig.c,
-     vertex.size=4,
-     vertex.color=V(ig.c)$pcolor,
-     vertex.label=NA,
-     edge.width=1,
-     layout=layout)
-```
-
-![](README_files/figure-gfm/unnamed-chunk-12-1.png)<!-- -->
-
-After traversing the network, we can define a ranked signature for this
-community - sort of like an extended signature - based on the most
-traversed nodes in the network.
-
-``` r
-signature.ranked <- V(ig.c)$p
-names(signature.ranked) <- V(ig.c)$name
-head(sort(signature.ranked, decreasing=TRUE))
-```
-
-    #>      EIF1B        ELL     NUDT21      ERCC5      SIAH1     SNAPC5 
-    #> 0.02916435 0.02722562 0.02642275 0.02362867 0.02315903 0.02314451
-
-This happens implicitly within this function.
-
-``` r
-hyp.c <- data$hyp
-```
-
-``` r
-hyp.c$plots$`Dna Repair`
-```
-
-![](README_files/figure-gfm/unnamed-chunk-15-1.png)<!-- -->
-
-``` r
-hypeR::hyp_dots(hyp.c)
-```
-
-![](README_files/figure-gfm/unnamed-chunk-16-1.png)<!-- -->
-
-The most important parameter will be the `restart` probability of the
-random walks. This tells the algorithm how long the walks should be and
-is the probability of the random walk restarting from the community. In
-otherwords, this will define how fuzzy your extended signature for a
-given community will be.
-
-``` r
-par(mar=c(0,0,0,0), mfrow=c(2,3))
-for (restart in c(0.75, 0.5, 0.25, 0.1, 0.05, 0)) {
-    data <- do_community(ig, community="16", genesets, restart=restart)
-    ig.c <- data$ig
-    plot(ig.c,
-         vertex.size=4,
-         vertex.color=V(ig.c)$pcolor,
-         vertex.label=NA,
-         edge.width=1,
-         layout=layout)
-    title(paste0("restart = ", restart), cex.main=2, font.main=4, line=-3)
-}
-```
-
-![](README_files/figure-gfm/unnamed-chunk-17-1.png)<!-- -->
-
-### Visualizing Results
-
-One way to visualize enrichment of the network would be to repeat the
-above process for each community
-
-``` r
-communities <- names(table(V(ig)$community))
-lhyp <- mapply(function(community) {
-    data <- do_community(ig, community=community, genesets, restart=0.5)
-    return(data$hyp)
-}, communities, SIMPLIFY=FALSE, USE.NAMES=FALSE)
-names(lhyp) <- paste0("C", communities)
-
-mhyp <- hypeR::multihyp$new(data=lhyp)
-hypeR::hyp_dots(mhyp, merge=TRUE, top=50, fdr=0.01)
-```
-
-![](README_files/figure-gfm/unnamed-chunk-18-1.png)<!-- -->
-
-Although we can also visualize the enrichment on the graph itself, which
-will give some perspective on why some communities close to each other
-might be enriched for similar pathways.
-
-``` r
-ig.e <- do_enrichment(ig, genesets, restart=0.5, fdr=0.01, top=1)
-```
-
-``` r
-par(mar=c(0,0,0,0))
-set.seed(2)
+set.seed(1234)
 plot(ig.e,
      vertex.size=5,
-     vertex.label=V(ig.e)$annotation,
-     vertex.color=adjustcolor(V(ig.e)$color, alpha.f=0.4),
-     vertex.frame.color=adjustcolor("#000000", alpha.f=0.1),
-     vertex.label.family="Helvetica",
-     vertex.label.color="black",
-     vertex.label.font=2,
-     vertex.label.cex=0.7,
-     vertex.label.dist=runif(vcount(ig.e), -1, 1),
-     edge.width=0,
-     layout=layout)
-```
-
-![](README_files/figure-gfm/unnamed-chunk-20-1.png)<!-- -->
-
-### Signature Extension
-
-Using similar methods, we can take a pre-defined signature, and
-contextualize it based on the existing network structure. We can see
-which signature genes are present in the network, which signature genes
-are most central, or which non-signature genes are within close
-proximity.
-
-Let’s pretend our signature happened to be a bunch of genes one one of
-our simulated communities…
-
-``` r
-signature <- c("ATP2B1", "KLF6", "IL1A", "CD69", "GCH1", "MXD1", "F3")
-```
-
-``` r
-data <- do_signature(ig, signature, restart=0.5)
-```
-
-Here we can visualize the propagation throughout the network from the
-signature.
-
-``` r
-ig.e <- data$ig
-
-par(mar=c(0,0,0,0))
-plot(ig.e,
-     vertex.size=4,
-     vertex.color=V(ig.e)$pcolor,
-     vertex.label=NA,
-     edge.width=1,
-     layout=layout)
-```
-
-![](README_files/figure-gfm/unnamed-chunk-23-1.png)<!-- -->
-
-Here we return the new signature based on the ranked propagation from
-the original signature.
-
-``` r
-df <- data$signature %>%
-      data.frame(name=names(.), p=.) %>%
-      dplyr::arrange(desc(p)) %>%
-      dplyr::mutate(np=normalize_range(p, 0, 1)) %>%
-      dplyr::mutate(original_signature=name %in% signature)
-
-head(df, 10)
-```
-
-    #>          name          p        np original_signature
-    #> KLF6     KLF6 0.09076717 1.0000000               TRUE
-    #> MXD1     MXD1 0.08970336 0.9882798               TRUE
-    #> F3         F3 0.08768029 0.9659912               TRUE
-    #> GCH1     GCH1 0.08309481 0.9154722               TRUE
-    #> CD69     CD69 0.08086103 0.8908621               TRUE
-    #> ATP2B1 ATP2B1 0.07985923 0.8798251               TRUE
-    #> IL1A     IL1A 0.07906760 0.8711036               TRUE
-    #> CCL20   CCL20 0.03224921 0.3552960              FALSE
-    #> YRDC     YRDC 0.03119150 0.3436430              FALSE
-    #> IL6       IL6 0.02915137 0.3211665              FALSE
-
-The original signature will likely have the most propagation values
-(because that is the seed), but we can find other genes with high
-propagation values close to the signature within the network.
-
-``` r
-p <- df %>%
-    dplyr::filter(!original_signature) %>%
-    head(25) %>%
-    dplyr::mutate(name=factor(name, levels=name)) %>%
-    ggplot(aes(x=name, y=np, fill=p))  +
-    geom_bar(stat="identity") +
-    labs(x="", y="Normalized Propogation") +
-    scale_fill_viridis_c() +
-    theme(axis.text.x=element_text(angle=45, vjust=1, hjust=1))
-
-p
-```
-
-![](README_files/figure-gfm/unnamed-chunk-25-1.png)<!-- -->
-
-``` r
-par(mar=c(0,0,0,0))
-plot(ig.e,
-     vertex.size=3,
-     vertex.color=V(ig.e)$pcolor,
-     vertex.frame.color=adjustcolor("#000000", alpha.f=0.5),
-     vertex.label=ifelse(V(ig.e)$p > 0.01, V(ig.e)$name, NA),
+     vertex.label=V(ig.e)$enrichment,
+     vertex.color=adjustcolor(V(ig.e)$color, alpha.f=0.6),
+     vertex.frame.color=adjustcolor("#000000", alpha.f=0),
      vertex.label.family="Helvetica",
      vertex.label.color="black",
      vertex.label.font=2,
      vertex.label.cex=0.8,
-     edge.width=1,
+     vertex.label.dist=runif(vcount(ig.e), -0.25, 0.25),
+     edge.color=adjustcolor("#000000", alpha.f=0.2),
      layout=layout)
 ```
 
-![](README_files/figure-gfm/unnamed-chunk-26-1.png)<!-- -->
+![](README_files/figure-gfm/unnamed-chunk-10-1.png)<!-- -->
+
+Please read the documentation for a comprehensive overview of
+functionality.
